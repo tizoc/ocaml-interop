7568050f30a0658cc51efefe16b28b719601d596
Bruno Deferrari <utizoc@gmail.com>
2020-07-31 17:46:27 -0300

    Expose less in the public API

--- src/mlvalues/tag.rs
+++ src/mlvalues/tag.rs
@@ -1,13 +1,13 @@
 pub type Tag = u8;
 
 pub const NO_SCAN: Tag = 251;
-pub const FORWARD: Tag = 250;
-pub const INFIX: Tag = 249;
-pub const OBJECT: Tag = 248;
+// pub const FORWARD: Tag = 250;
+// pub const INFIX: Tag = 249;
+// pub const OBJECT: Tag = 248;
 pub const CLOSURE: Tag = 247;
-pub const LAZY: Tag = 246;
-pub const ABSTRACT: Tag = 251;
+// pub const LAZY: Tag = 246;
+// pub const ABSTRACT: Tag = 251;
 pub const STRING: Tag = 252;
-pub const DOUBLE: Tag = 253;
-pub const DOUBLE_ARRAY: Tag = 254;
-pub const CUSTOM: Tag = 255;
+// pub const DOUBLE: Tag = 253;
+// pub const DOUBLE_ARRAY: Tag = 254;
+// pub const CUSTOM: Tag = 255;
