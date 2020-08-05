e7461a3a21583616e789469a073bc057097923e7
Bruno Deferrari <utizoc@gmail.com>
2020-08-05 11:18:51 -0300

    Make OCamlRef public

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ mod value;
 pub use crate::closure::{OCamlClosure, OCamlResult};
 pub use crate::conv::{FromOCaml, ToOCaml, ToOCamlInteger};
 pub use crate::error::Error;
-pub use crate::memory::{GCFrame, GCResult, GCToken};
+pub use crate::memory::{GCFrame, GCResult, GCToken, OCamlRef};
 pub use crate::mlvalues::{Intnat, RawOCaml};
 pub use crate::runtime::init as init_runtime;
 pub use crate::runtime::shutdown as shutdown_runtime;
