5c2930e71057f92ad1626fc6cefe10f3a4ba55d7
Bruno Deferrari <utizoc@gmail.com>
2020-08-07 09:50:42 -0300

    Move exports that shouldn't be used directly to an internal submodule

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,19 @@ mod mlvalues;
 mod runtime;
 mod value;
 
-pub use crate::closure::{OCamlClosure, OCamlResult};
+pub use crate::closure::OCamlResult;
 pub use crate::conv::{FromOCaml, ToOCaml, ToOCamlInteger};
 pub use crate::error::Error;
-pub use crate::memory::{GCFrame, GCResult, GCToken, OCamlRef};
+pub use crate::memory::OCamlRef;
 pub use crate::mlvalues::{Intnat, RawOCaml};
 pub use crate::runtime::init as init_ocaml_runtime;
 pub use crate::runtime::shutdown as shutdown_ocaml_runtime;
 pub use crate::value::OCaml;
 
+pub mod internal {
+    pub use crate::closure::OCamlClosure;
+    pub use crate::memory::{GCFrame, GCResult, GCToken};
+}
 
 #[cfg(doctest)]
 pub mod compile_fail_tests;
--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! ocaml_frame {
     ($gc:ident, $body:block) => {{
-        let mut frame: $crate::GCFrame = Default::default();
+        let mut frame: $crate::internal::GCFrame = Default::default();
         let $gc = frame.initialize();
         $body
     }};
@@ -12,7 +12,7 @@ macro_rules! ocaml {
     () => ();
 
     ($vis:vis fn $name:ident($arg:ident: $typ:ty) -> $rtyp:ty; $($t:tt)*) => {
-        $vis unsafe fn $name(token: $crate::GCToken, $arg: $crate::OCaml<$typ>) -> $crate::OCamlResult<$rtyp> {
+        $vis unsafe fn $name(token: $crate::internal::GCToken, $arg: $crate::OCaml<$typ>) -> $crate::OCamlResult<$rtyp> {
             $crate::ocaml_closure_reference!(F, $name);
             F.call(token, $arg)
         }
@@ -21,7 +21,7 @@ macro_rules! ocaml {
     };
 
     ($vis:vis fn $name:ident($arg1:ident: $typ1:ty, $arg2:ident: $typ2:ty) -> $rtyp:ty; $($t:tt)*) => {
-        $vis unsafe fn $name(token: $crate::GCToken, $arg1: $crate::OCaml<$typ1>, $arg2: $crate::OCaml<$typ2>) -> $crate::OCamlResult<$rtyp> {
+        $vis unsafe fn $name(token: $crate::internal::GCToken, $arg1: $crate::OCaml<$typ1>, $arg2: $crate::OCaml<$typ2>) -> $crate::OCamlResult<$rtyp> {
             $crate::ocaml_closure_reference!(F, $name);
             F.call2(token, $arg1, $arg2)
         }
@@ -30,7 +30,7 @@ macro_rules! ocaml {
     };
 
     ($vis:vis fn $name:ident($arg1:ident: $typ1:ty, $arg2:ident: $typ2:ty, $arg3:ident: $typ3:ty) -> $rtyp:ty; $($t:tt)*) => {
-        $vis unsafe fn $name(token: $crate::GCToken, $arg1: $crate::OCaml<$typ1>, $arg2: $crate::OCaml<$typ2>, $arg3: $crate::OCaml<$typ3>) -> $crate::OCamlResult<$rtyp> {
+        $vis unsafe fn $name(token: $crate::internal::GCToken, $arg1: $crate::OCaml<$typ1>, $arg2: $crate::OCaml<$typ2>, $arg3: $crate::OCaml<$typ3>) -> $crate::OCamlResult<$rtyp> {
             $crate::ocaml_closure_reference!(F, $name);
             F.call3(token, $arg1, $arg2, $arg3)
         }
@@ -39,7 +39,7 @@ macro_rules! ocaml {
     };
 
     ($vis:vis fn $name:ident($($arg:ident: $typ:ty),+) -> $rtyp:ty; $($t:tt)*) => {
-        $vis unsafe fn $name(token: $crate::GCToken, $($arg: $crate::OCaml<$typ>),+) -> $crate::OCamlResult<$rtyp> {
+        $vis unsafe fn $name(token: $crate::internal::GCToken, $($arg: $crate::OCaml<$typ>),+) -> $crate::OCamlResult<$rtyp> {
             $crate::ocaml_closure_reference!(F, $name);
             F.call_n(token, &mut [$($arg.into()),+])
         }
@@ -131,9 +131,9 @@ macro_rules! ocaml_call {
 macro_rules! ocaml_closure_reference {
     ($var:ident, $name:ident) => {
         static name: &str = stringify!($name);
-        static mut OC: Option<$crate::OCamlClosure> = None;
+        static mut OC: Option<$crate::internal::OCamlClosure> = None;
         if OC.is_none() {
-            OC = $crate::OCamlClosure::named(name);
+            OC = $crate::internal::OCamlClosure::named(name);
         }
         let $var =
             OC.unwrap_or_else(|| panic!("OCaml closure with name '{}' not registered", name));
