364d89694417d34ac521724214f71c130fa0f669
Bruno Deferrari <utizoc@gmail.com>
2020-08-17 17:13:48 -0300

    Add IntoRust trait (counterpart to FromOCaml)

--- src/conv.rs
+++ src/conv.rs
@@ -1,5 +1,7 @@
 mod from_ocaml;
 mod to_ocaml;
+mod into_rust;
 
 pub use self::from_ocaml::FromOCaml;
 pub use self::to_ocaml::ToOCaml;
+pub use self::into_rust::IntoRust;
--- testing/ocaml-caller/rust/src/lib.rs
+++ testing/ocaml-caller/rust/src/lib.rs
@@ -1,14 +1,15 @@
-use znfe::{ocaml_alloc, ocaml_export, FromOCaml, Intnat, OCaml, OCamlBytes, OCamlList, ToOCaml};
+use znfe::{ocaml_alloc, ocaml_export, IntoRust, Intnat, OCaml, OCamlBytes, OCamlList, ToOCaml};
 
 ocaml_export! {
     fn rust_twice(_gc, num: OCaml<Intnat>) -> OCaml<Intnat> {
-        let num = i64::from_ocaml(num);
+        let num: i64 = num.into_rust();
         OCaml::of_int(num * 2)
     }
 
     fn rust_increment_bytes(gc, bytes: OCaml<OCamlBytes>, first_n: OCaml<Intnat>) -> OCaml<OCamlBytes> {
-        let first_n = i64::from_ocaml(first_n) as usize;
-        let mut vec = Vec::from_ocaml(bytes);
+        let first_n: i64 = first_n.into_rust();
+        let first_n = first_n as usize;
+        let mut vec: Vec<u8> = bytes.into_rust();
 
         for i in 0..first_n {
             vec[i] += 1;
@@ -18,7 +19,7 @@ ocaml_export! {
     }
 
     fn rust_increment_ints_list(gc, ints: OCaml<OCamlList<Intnat>>) -> OCaml<OCamlList<Intnat>> {
-        let mut vec = <Vec<i64>>::from_ocaml(ints);
+        let mut vec: Vec<i64> = ints.into_rust();
 
         for i in 0..vec.len() {
             vec[i] += 1;
@@ -28,14 +29,14 @@ ocaml_export! {
     }
 
     fn rust_make_tuple(gc, fst: OCaml<String>, snd: OCaml<Intnat>) -> OCaml<(String, Intnat)> {
-        let fst = String::from_ocaml(fst);
-        let snd = i64::from_ocaml(snd);
+        let fst: String = fst.into_rust();
+        let snd: i64 = snd.into_rust();
         let tuple = (fst, snd);
         ocaml_alloc!(tuple.to_ocaml(gc))
     }
 
     fn rust_make_some(gc, value: OCaml<String>) -> OCaml<Option<String>> {
-        let value = String::from_ocaml(value);
+        let value: String = value.into_rust();
         let some_value = Some(value);
         ocaml_alloc!(some_value.to_ocaml(gc))
     }
