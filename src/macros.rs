fb13bc5220a522fcb538825b19e055b7e73aa987
Bruno Deferrari <utizoc@gmail.com>
2020-09-30 13:47:46 -0300

    Fix tag check

--- src/macros.rs
+++ src/macros.rs
@@ -860,7 +860,7 @@ macro_rules! unpack_variant_tag {
     };
 
     ($self:ident, $current_block_tag:ident, $current_long_tag:ident, $($tag:ident)::+ => $conv:expr) => {
-        if $self.is_long() && unsafe { $self.raw() } == $current_long_tag {
+        if $self.is_long() && $self.as_i64() == $current_long_tag {
             return Ok($conv);
         }
         $current_long_tag += 1;
