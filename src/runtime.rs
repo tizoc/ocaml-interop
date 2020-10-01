a636e311dbf153212c393cc36193bb81aef095ec
Bruno Deferrari <utizoc@gmail.com>
2020-10-01 17:58:03 -0300

    Docs for OCamlRuntime

--- src/runtime.rs
+++ src/runtime.rs
@@ -10,17 +10,21 @@ extern "C" {
 pub struct OCamlRuntime {}
 
 impl OCamlRuntime {
+    /// Initializes the OCaml runtime and returns a handle, that once dropped
+    /// will perform the necessary cleanup.
     pub fn init() -> Self {
         OCamlRuntime::init_persistent();
         OCamlRuntime {}
     }
 
+    /// Initializes the OCaml runtime.
     pub fn init_persistent() {
         let arg0 = "ocaml".as_ptr() as *const i8;
         let c_args = vec![arg0, std::ptr::null()];
         unsafe { caml_startup(c_args.as_ptr()) }
     }
 
+    /// Performs the necessary cleanup and shuts down the OCaml runtime.
     pub fn shutdown_persistent() {
         unsafe { caml_shutdown() }
     }
