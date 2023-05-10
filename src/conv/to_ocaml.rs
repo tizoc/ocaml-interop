f9ecf016e725589a402f3d61e120d96b8284d506
Sebastien Chapuis <sebastien@chapu.is>
2023-05-10 15:20:33 +0200

    Add implementation for bigstring

--- src/conv/to_ocaml.rs
+++ src/conv/to_ocaml.rs
@@ -143,6 +143,13 @@ unsafe impl ToOCaml<OCamlBytes> for Box<[u8]> {
     }
 }
 
+unsafe impl ToOCaml<Array1<u8>> for Box<[u8]> {
+    fn to_ocaml<'a>(&self, cr: &'a mut OCamlRuntime) -> OCaml<'a, Array1<u8>> {
+        let slice: &[u8] = self;
+        slice.to_ocaml(cr)
+    }
+}
+
 unsafe impl<A, OCamlA> ToOCaml<OCamlA> for Box<A>
 where
     A: ToOCaml<OCamlA>,
